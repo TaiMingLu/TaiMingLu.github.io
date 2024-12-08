c836af5f088147a0dfde5b55daea1d1d79d88685
TaiMingLu <terrytaimingl@gmail.com>
2024-12-07 20:25:56 -0500

    fix module issues

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,9 @@
 import { initializeThemeController, setupThemeController } from './theme-controller.js';
-// If you load favicon-controller.js and paper-split-controller.js via script tags in HTML, no need to import here.
-// If you prefer to load them as modules, you could do:
-// import './favicon-controller.js';
-// import './paper-split-controller.js';
+import { applyColorsBasedOnTheme } from './paper-split-controller.js';
+
 
 document.addEventListener('DOMContentLoaded', () => {
     initializeThemeController();
     setupThemeController();
+    applyColorsBasedOnTheme();
 });
